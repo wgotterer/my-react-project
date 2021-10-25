1a2c035c1f89cd62405a532e5627e423104b83a0
William Gotterer <william.gotterer@gmail.com>
2021-10-25 18:05:55 -0400

    changes

--- src/components/Favorites.js
+++ src/components/Favorites.js
@@ -1,22 +1,14 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 
 function Favorites() {
+
+  
+
+
   return (
     <div>
       <h1> Favorites List </h1>
     </div>
-    // <div>
-    //   <h1>Login</h1>
-    //   <form>
-    //     <div>
-    //       <input type="text" name="username" placeholder="Username" />
-    //     </div>
-    //     <div>
-    //       <input type="password" name="password" placeholder="Password" />
-    //     </div>
-    //     <input type="submit" value="Submit" />
-    //   </form>
-    // </div>
   );
 }
 
