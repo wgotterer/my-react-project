ca297edec4d8c2cda6b7ed0a5cf993d20a266e0a
William Gotterer <william.gotterer@gmail.com>
2021-10-25 12:34:54 -0400

    Routes

--- src/components/App.js
+++ src/components/App.js
@@ -2,10 +2,10 @@
 
 import React from "react";
 import { Route, Switch } from "react-router-dom";
-import Home from "./components/Home";
-import About from "./components/About";
-import Login from "./components/Login";
-import NavBar from "./components/NavBar";
+import Home from "./Home";
+import About from "./About";
+import Login from "./Login";
+import NavBar from "./NavBar";
 
 function App() {
   return (
