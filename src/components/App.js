32863080e7949579651e54ba190e0edfad8f5181
William Gotterer <william.gotterer@gmail.com>
2021-10-25 12:23:21 -0400

    trying to make things work

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,16 @@
 
-import './App.css';
+
 import React from "react";
-// import { Route, Switch } from "react-router-dom";
-// import Home from "./Home";
-// import About from "./About";
-// import Login from "./Login";
-// import NavBar from "./NavBar";
+import { Route, Switch } from "react-router-dom";
+import Home from "./components/Home";
+import About from "./components/About";
+import Login from "./components/Login";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
     <div>
-      HELP!
-      {/* <NavBar />
+      <NavBar />
       <Switch>
         <Route exact path="/about">
           <About />
@@ -22,7 +21,7 @@ function App() {
         <Route exact path="/">
           <Home />
         </Route>
-      </Switch> */}
+      </Switch>
     </div>
   );
 }
